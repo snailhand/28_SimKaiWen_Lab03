3799662da40172a575bf52d72b123fb0ef88c802
simkaiwen <kaiphonegt@gmail.com>
2022-01-13 17:45:06 +0800

    Increased Speed for Level 2

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -26,13 +26,19 @@ public class Spawner : MonoBehaviour
 
     void spawnobject()
     {
-       
-        
         randomInt = Random.Range(0, spawnee.Length);
         positionx = Random.Range(8f,-8f);
         this.transform.position = new Vector3(positionx,transform.position.y , transform.position.z);
-        Instantiate(spawnee[randomInt], transform.position, transform.rotation);
-   
+        var spawnedObject = Instantiate(spawnee[randomInt], transform.position, transform.rotation);
+        
+        if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "GamePlay_Level 2")
+        {
+            IncreaseObjectSpeed(spawnedObject);
+        }
+    }
 
+    void IncreaseObjectSpeed(GameObject obj)
+    {
+        obj.GetComponent<ObjectMovement>().ySpeed *= Random.Range(1.5f,2.5f);
     }
 }
