cda99aec5613c908cbd92060496069ffb586cb88
simkaiwen <kaiphonegt@gmail.com>
2022-01-13 16:41:43 +0800

    Changes to BasketMovementScript
    - Restricted movement
    - Collision detection

--- Assets/Scripts/BasketMovementScript.cs
+++ Assets/Scripts/BasketMovementScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BasketMovementScript : MonoBehaviour
 {
     public float speed;
+    public float leftBoundary, rightBoundry;
    
     // Start is called before the first frame update
     void Start()
@@ -15,18 +16,44 @@ public class BasketMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-      float horizontalInput = Input.GetAxis("Horizontal");
-
-      transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
-
-        
-       
-
+        MoveBasket();
     }
 
+    private void MoveBasket()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
 
+        if (transform.position.x < rightBoundry)
+        {
+            transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(rightBoundry, transform.position.y, transform.position.z);
+        }
 
-    
+        if (transform.position.x > leftBoundary)
+        {
+            transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.position = new Vector3(leftBoundary, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Healthy")
+        {
+            //Add points
+
+        }
+        else if(collision.gameObject.tag == "Unhealthy")
+        {
+            //Minus points
+
+        }
+    }
 
 }
